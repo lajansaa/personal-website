d394d70d93c4c4a72d006bb3afba001ee2dcbc52
Isa <lajansa@hotmail.com>
2018-05-10 17:37:44 +0800

    home page and main actions

---
+++ script.js
@@ -0,0 +1,8 @@
+function leftSlideIn() {
+  $('#left-arrow').css({"-webkit-animation": "slide 0.5s forwards",
+                          "-webkit-animation-delay": "2s",
+                          "animation": "slide 0.5s forwards",
+                          "animation-delay": "3s"
+                        });
+}
+
